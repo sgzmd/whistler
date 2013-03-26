c59a49e116601745d98c83a927a814748a68bd5a
sgzmd <sigizmund@gmail.com>
2013-03-26 20:51:34 +0000

    Initial import, capable of building Arduino code

---
+++ arduino/whistler-arduino/firmware/RF24/RF24.d
@@ -0,0 +1,13 @@
+RF24/RF24.o: RF24/RF24.cpp RF24/nRF24L01.h RF24/RF24_config.h \
+  //Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Arduino.h \
+  //Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/binary.h \
+  //Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/WCharacter.h \
+  //Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/WString.h \
+  //Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/HardwareSerial.h \
+  //Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Stream.h \
+  //Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Print.h \
+  //Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Printable.h \
+  //Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/new.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/variants/mega/pins_arduino.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/libraries/SPI/SPI.h \
+  RF24/RF24.h RF24/RF24_config.h
