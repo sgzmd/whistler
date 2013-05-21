20837bf81de7f5e96ef1447d7ed78127c2428811
sgzmd <sigizmund@gmail.com>
2013-05-21 22:01:16 +0100

    Everything is really working for old ArPiMessage

--- common/arpix/arpix.h
+++ common/arpix/arpix.h
@@ -1,20 +1,10 @@
-// Author: Roman "sgzmd" Kirillov [me@romankirillov.info]
-
 #ifndef __ARPIX_H
 #define __ARPIX_H
 
-#include <stdlib.h>
-
 struct ArPiMessage {
-  ArPiMessage() : sender_id(0), data(0), parity(0) {}
-
-  ArPiMessage(unsigned long _sender, unsigned long _data) :
-    sender_id(_sender), data(_data), parity(0) {}
   unsigned long sender_id;
   unsigned long data;
   char parity;
 };
-
-int dump_to_string(const ArPiMessage& msg, char* buffer, size_t length);
 
 #endif
