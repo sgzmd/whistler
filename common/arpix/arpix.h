48bf4005675e660c7846e3fd7a313688e44ed048
sgzmd <sigizmund@gmail.com>
2013-05-28 21:18:12 +0100

    Revert "Backporting changes to Arpix from other branches"
    This reverts commit 0bb4fd28ca506fa6098279638dbcf6c995436869.

--- common/arpix/arpix.h
+++ common/arpix/arpix.h
@@ -1,20 +1,16 @@
-// Author: Roman "sgzmd" Kirillov [me@romankirillov.info]
-
 #ifndef __ARPIX_H
 #define __ARPIX_H
 
-#include <stdlib.h>
+#ifndef size_t
+#define size_t unsigned int
+#endif
 
 struct ArPiMessage {
-  ArPiMessage() : sender_id(0), data(0), parity(0) {}
-
-  ArPiMessage(unsigned long _sender, unsigned long _data) :
-    sender_id(_sender), data(_data), parity(0) {}
   unsigned long sender_id;
   unsigned long data;
   char parity;
 };
 
-int dump_to_string(const ArPiMessage& msg, char* buffer, size_t length);
+int dump_to_string(const ArPiMessage* msg, char* buffer, unsigned int length);
 
 #endif
