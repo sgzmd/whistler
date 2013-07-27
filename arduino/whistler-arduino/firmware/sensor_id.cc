561522433b4488a1ca0f7e945212ed01d94446d1
sgzmd <sigizmund@gmail.com>
2013-07-27 21:26:04 +0100

    Reading and writing sensor ID to EEPROM. Not tested.
    Tried to test it but stuck with programmer not responding error. Might need to re-test it later.

---
+++ arduino/whistler-arduino/firmware/sensor_id.cc
@@ -0,0 +1,18 @@
+#include "sensor_id.h"
+
+#include <EEPROM.h>
+#include <Arduino.h>
+
+
+const unsigned int BOARD_ID_ADDRESS = 0;
+const unsigned int UNASSSIGNED = 0xFF;
+
+unsigned char get_board_id() {
+  unsigned char board_id = EEPROM.read(BOARD_ID_ADDRESS);
+  if (UNASSSIGNED == board_id) {
+    board_id = random(255);
+    EEPROM.write(BOARD_ID_ADDRESS, board_id);
+  }
+
+  return board_id;
+}
---
+++ arduino/whistler-arduino/firmware/sensor_id.h
@@ -0,0 +1,6 @@
+#ifndef __SENSOR_ID_H
+#define __SENSOR_ID_H
+
+unsigned char get_board_id();
+
+#endif
